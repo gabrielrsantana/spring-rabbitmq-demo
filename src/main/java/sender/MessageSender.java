9c2e1eb96188db6f4c88c162ac7cd42590b1b752
Gabriel <gabriel.rsantana@hotmail.com>
2025-05-14 18:23:12 -0300

    criação do sender Service

---
+++ src/main/java/sender/MessageSender.java
@@ -0,0 +1,20 @@
+package sender;
+
+import config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageSender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void send(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE, message);
+    }
+}
+

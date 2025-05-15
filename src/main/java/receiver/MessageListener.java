68e4a81093040d821e971a2090221410f35dab6a
Gabriel <gabriel.rsantana@hotmail.com>
2025-05-15 09:23:13 -0300

    criando mesage listener

---
+++ src/main/java/receiver/MessageListener.java
@@ -0,0 +1,14 @@
+package receiver;
+
+import config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE)
+    public void listen(String message) {
+        System.out.println("Recebido: " + message);
+    }
+}
